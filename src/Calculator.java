792cede191b52d06b5de86b70cb77ebf29881539
Catalin <katalinus_katty@yahoo.com>
2022-11-14 11:34:31 +0200

    Calc

---
+++ src/Calculator.java
@@ -0,0 +1,39 @@
+public class Calculator {
+
+    public int operAdunare(int a, int b ){
+        return a + b;
+    }
+    public int operScadere(int a, int b){
+        return a - b;
+    }
+    public int operInmultire(int a, int b){
+        return a * b;
+    }
+    public float operImpartire(float a, float b){
+        return a / b;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,49 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator Calc = new Calculator();
+
+        int nrZero      = 0;
+        int nrUnu       = 1;
+        int nrDoi       = 2;
+        int nrUnspe     = 11;
+        int a           = 20;
+        int b           = 15;
+        int x           = 4;
+        int nrOsuta     = 100;
+        int nrOsutaZece = 110;
+
+
+        int adunare = Calc.operAdunare(a,b);
+        System.out.println(adunare);
+        int scadere = Calc.operScadere(a,b);
+        System.out.println(scadere);
+        int inmultire = Calc.operInmultire(a,b);
+        System.out.println(inmultire);
+        float impartire = Calc.operImpartire(a,b);
+        System.out.println(impartire);
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
