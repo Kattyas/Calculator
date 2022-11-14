b66092a494254a4ea4856489106147e97f87e355
Catalin <katalinus_katty@yahoo.com>
2022-11-14 15:59:21 +0200

    Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,6 @@
 public class Calculator {
 
+
     public int operAdunare(int a, int b ){
         return a + b;
     }
@@ -12,26 +13,6 @@ public class Calculator {
     public float operImpartire(float a, float b){
         return a / b;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
